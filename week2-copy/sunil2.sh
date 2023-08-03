2b2bc21b48e9a412d844a61917b105205fdce453
chadalavadasunil <139360983+chadalavadasunil@users.noreply.github.com>
2023-08-03 10:40:57 +0000

    Delete week2-copy directory

--- week2-copy/sunil2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -rp "enter first name::"  first
-read -rp "enter second name::"  second
-echo "hi $first $second  how are you?"
-read -rsp "enter the password" password
-echo " i know ur password $password"
-exit 0
--- week2-copy/week2/folder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "enter the folder name"
-read a
-mkdir $a
-chmod 777 $a 
-rmdir   $a
-echo " folder is created $a"
-exit 0 
