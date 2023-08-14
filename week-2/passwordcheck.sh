dd518fe3f309c51e6b7af4084b0d6d543d8a238c
chadalavadasunil <schadala@our.ecu.edu.au>
2023-08-14 06:02:09 +0000

    week-2 script

--- week-2/passwordcheck.sh
+++ week-2/passwordcheck.sh
@@ -3,7 +3,7 @@ stored_hash=$(cat password_hash.txt)
 # Prompt the user for  password :password is sunil
 read -r -s -p "Enter your password: " user_password
 echo
-# SHA-256 check 
+##SHA-256 check 
 hashed_user_password=$(printf '%s' "$user_password" | sha256sum | awk '{print $1}')
 
 # Check if the entered password matches the stored hash
